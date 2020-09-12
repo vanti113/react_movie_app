f55a7764cb8317eb57f2534df587287ce9466ff1
Bae kigun <vanti113@gmail.com>
2020-09-13 00:06:11 +0900

    Update precache-manifest.2abf0b8e05fb0edde67a5b29815da756.js

--- precache-manifest.2abf0b8e05fb0edde67a5b29815da756.js
+++ precache-manifest.2abf0b8e05fb0edde67a5b29815da756.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f75e3065970a2a3eca9954848ad2a18f",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "0d199711d40899713cbd",
-    "url": "/static/css/main.5a0f0ca7.chunk.css"
+    "url": "./static/css/main.5a0f0ca7.chunk.css"
   },
   {
     "revision": "a7d22b2c00d8d326c863",
-    "url": "/static/js/2.300fc6d8.chunk.js"
+    "url": "./static/js/2.300fc6d8.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.300fc6d8.chunk.js.LICENSE.txt"
   },
   {
     "revision": "0d199711d40899713cbd",
-    "url": "/static/js/main.c8dc659b.chunk.js"
+    "url": "./static/js/main.c8dc659b.chunk.js"
   },
   {
     "revision": "bd7f4f55b55e38cea2f1",
-    "url": "/static/js/runtime-main.58002af7.js"
+    "url": "./static/js/runtime-main.58002af7.js"
   }
 ]);
